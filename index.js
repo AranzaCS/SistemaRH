73ae6e1cabc8cfb369c0df5c08524a4ba0528486
Aranza Cordero <acordero21@alumnos.uaq.mx>
2023-05-22 19:09:02 -0600

    added db

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 //Dependencies
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
 //Routers
+const employee = require('./routes/employee');
+const user = require('./routes/user');
 //Middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
+const cors = require('./middleware/cors');
+
+app.use(cors);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', index);
+app.use('/user', user);
+app.use(auth);
+app.use('/employee', employee);
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
---
+++ routes/employee.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const employee = express.Router();
+const db = require('../config/database');
+
+employee.post('/', async (req, res, next) => {
+    const { emp_id, emp_name, emp_surnames, emp_phone, emp_email, emp_address } = req.body;
+    if (emp_id && emp_name && emp_surnames && emp_phone && emp_email && emp_address) {
+        let query = "INSERT INTO employees(emp_id, emp_name, emp_surnames, emp_phone, emp_email, emp_address)";
+        query += `VALUES ('${emp_id}', ${emp_name}, ${emp_surnames}, ${emp_phone}, '${emp_email}', '${emp_address}')`;
+        const rows = await db.query(query);
+
+        (rows.affectedRows == 1) ?
+            res.status(201).json({ code: 201, message: "Empleado insertado correctamente" }) :
+            res.status(200).json({ code: 500, message: "Ocurrio un error" });
+    }
+    return res.status(200).json({ code: 500, message: "Campos incompletos" });
+});
+
+employee.delete('/:id([0-9]{1,3})', async (req, res, next) => {
+    const query = `DELETE FROM employees WHERE emp_id=${req.params.id}`;
+    const rows = await db.query(query);
+
+    rows.affectedRows == 1 ?
+        res.status(200).json({ code: 200, message: "Empleado borrado correctamente" }) :
+        res.status(404).json({ code: 404, message: "Empleado no encontrado" });
+});
+
+employee.put('/:id([0-9]{1,3})', async (req, res, next) => {
+    const { emp_id, emp_name, emp_surnames, emp_phone, emp_email, emp_address } = req.body;
+
+    if (emp_id && emp_name && emp_surnames && emp_phone && emp_email && emp_address) {
+        let query = `UPDATE employees SET emp_id='${emp_id}',emp_name='${emp_name}',emp_surnames='${emp_surnames}',emp_phone='${emp_phone}',emp_email='${emp_email}',emp_address='${emp_address}' WHERE emp_id=${req.params.id}`;
+
+        const rows = await db.query(query);
+
+        (rows.affectedRows == 1) ?
+            res.status(200).json({ code: 200, message: "Empleado actualizado correctamente" }) :
+            res.status(500).json({ code: 500, message: "Ocurrio un error" });
+    }
+    return res.status(500).json({ code: 500, message: "Campos incompletos" });
+});
+
+employee.patch('/:id([0-9]{1,3})', async (req, res, next) => {
+    if (req.body.emp_name) {
+        let query = `UPDATE employees SET emp_name='${req.body.emp_name}' WHERE emp_id=${req.params.id}`;
+        const rows = await db.query(query);
+
+        (rows.affectedRows == 1) ?
+            res.status(200).json({ code: 200, message: "Empleado actualizado correctamente" }) :
+            res.status(500).json({ code: 500, message: "Ocurrio un error" });
+    }
+    return res.status(500).json({ code: 500, message: "Campos incompletos"});
+});
+
+employee.get('/', async (req,res,next) => {
+    const rts = await db.query('SELECT * FROM employees');
+    return res.status(200).json({ code: 200, message: rts });
+});
+
+employee.get('/:id([0-9]{1,3})', async (req, res, next) => {
+    const id = req.params.id;
+    if (id >= 1 && id <= 50) {
+        const rts = await db.query("SELECT * FROM employees WHERE emp_id=" + id + ";");
+    }
+    return res.status(404).json({ code: 404, message: 'Empleado no encontrado' });
+});
+
+employee.get('/:name([A-Za-z]+)', async (req, res, next) => {
+    const name = req.params.name;
+    const rts = await db.query("SELECT * FROM employees WHERE emp_name='" + name + "';");
+
+    (rts.length > 0) ?
+        res.status(200).json({ code: 200, message: rts }) :
+        res.status(404).json({ code: 404, message: 'Empleado no encontrado' });
+});
+
+module.exports = employee;
